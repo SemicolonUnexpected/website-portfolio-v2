19df59b4adf0070d352a6ae93f7fe1c1eb0e37d5
SemicolonUnexpected <m.gracey1409@gmail.com>
2024-08-12 10:19:54 +0000

    Cleaned up the home page to remove any distractions while I make the nav

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,6 @@ import Hero from "@/components/hero";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <Hero/>
-      </div>
-
-      <div>
-      </div>
     </main>
   );
 }
