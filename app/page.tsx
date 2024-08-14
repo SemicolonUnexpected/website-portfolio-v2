2d0dd6a4a65f2f0cc22b5b38070ef9dd7fc58653
SemicolonUnexpected <m.gracey1409@gmail.com>
2024-08-14 12:06:45 +0000

    Basically finished the mobile home page... hopefully no unnoticed layout bugs

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,31 @@
 import Hero from "@/components/hero";
+import { Mouse } from "@/components/mouse-art";
 import MouseCord from "@/components/mouse-cord";
 
 export default function Home() {
   return (
     <div className="flex relative min-h-screen flex-col items-center pt-10 w-[360px]">
-      <div className="z-10">
+      <div className="z-20">
         <Hero/>
       </div>
 
-      <MouseCord className="w-[500px] -translate-y-6 z-0"/>
+      <MouseCord className="w-[430px] -translate-y-3 z-10"/>
 
-      <div className="absolute top-[600px] left-[10px] bg-blue-500 w-96 h-52">
-        Hi, I&#39;m Matthew, aka SemicolonUnexpected. I am a budding computer scientist who is enthusiastic for mathematics and programming projects
+      <div className="absolute top-[445px] flex flex-col gap-40 text-center text-ellipsis overflow-hidden">
+        <div>
+          Hi, I&#39;m Matthew, aka SemicolonUnexpected. I am a budding computer scientist who is enthusiastic for mathematics and programming projects
+        </div>
+
+        <div>
+          I am self taught in C#, python and Typescript. I also have some experience with java in the context of the First Tech Challenge, and also some rust and haskell
+        </div>
+
+        <div>
+          Linux is my primary OS, and I am very comfortable using it. I also dual boot windows to get the best of both worlds
+        </div>
       </div>
 
-      <div>
-        I am self taught in C#, python and Typescript. I also have some experience with java in the context of the First Tech Challenge, and also some rust and haskell
-      </div>
-
-      <div>
-        Linux is my primary OS, and I am very comfortable using it. I also dual boot windows to get the best of both worlds
-      </div>
+      <Mouse className="absolute w-28 top-[792px] left-[221px] z-0"/>
     </div>
   );
 }
