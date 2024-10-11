56fca30c8a60bc563d9757e2fef03282647cdafe
SemicolonUnexpected <112770950+SemicolonUnexpected@users.noreply.github.com>
2024-10-11 16:33:20 +0000

    Update page.tsx

--- app/projects/robotics/page.tsx
+++ app/projects/robotics/page.tsx
@@ -11,13 +11,13 @@ export default function Robotics() {
         <p className="text-justify">
           Recently I had the good fortune to compete in the <Link href="https://www.firstinspires.org/robotics/ftc" text="First Tech Challenge"/> nationals at 
           Cambridge University after winning the Inspire Award at Northern Ireland regionals. This was an amazing 
-          experience, and allowed me to put my STEM skills to the test. Our team put in a huge amount of effort over the course of 
+          experience and allowed me to put my STEM skills to the test. Our team put in a huge amount of effort over 
           the whole year, and this was evident as we managed to finish as Division 1 finalists.
         </p>
         <Image src="/robot.png" alt="Robot" sizes="100vw" width="0" height="0" className="w-96 rounded-md"/>
         <p className="text-justify">
           I was in charge of programming our robotic arm and worked with the drivers to improve its ease of use. I orientated the wrist automatically 
-          and provided preset heights that could be reached at the click of a button. In addition to this, I limited the robot's 
+          and provided preset heights that could be reached at the click of a button. In addition to this, I limited the robot&#39;s 
           speed while the arm was extended to allow fine-grained control. Working with hardware was quite stressful as I had to ensure
           there were no bug in the code before I pushed it to the robot to prevent the robot from damaging itself. I ended up with the 
           simple code shown below. Based on which button the driver pressed, the 
